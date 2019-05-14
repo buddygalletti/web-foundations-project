0d720fea8b7f939406f284ec09c4e385615c590b
Buddy Galletti <buddygalletti@gmail.com>
2019-05-14 09:10:49 -0700

    Cleaned up some redundant code and built the DOM elements in main.js

--- main.js
+++ main.js
@@ -24,9 +24,22 @@ const data = {
 };
 
 
+const body = document.querySelector('body');
+const header = document.createElement('div');
+const sectionHeader1 = document.createElement('div');
+const sectionHeader2 = document.createElement('div');
+const flexBox1 = document.createElement('div');
+const flexBox2 = document.createElement('div');
+
+header.innerHTML = '<h1>Acme Prizes</h1>';
+sectionHeader1.innerHTML = '<h2>Prize Inventory</h2>'
+sectionHeader2.innerHTML = '<h2>Customers</h2>'
+
+body.appendChild(header).setAttribute('id', 'header');
+body.appendChild(sectionHeader1).classList.add('section-header');
+body.appendChild(flexBox1).classList.add('flex-container');
+body.appendChild(sectionHeader2).classList.add('section-header');
+body.appendChild(flexBox2).classList.add('flex-container');
 
 
 
-
-
-console.log('I have been loaded');
