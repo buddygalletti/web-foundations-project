f54915cb5653c3c559d040af6857499306836dd6
Buddy Galletti <buddygalletti@gmail.com>
2019-05-14 07:40:56 -0700

    Added some preliminary stuff

---
+++ main.js
@@ -0,0 +1,27 @@
+const data = {
+  customers: {
+    Moe: {
+      Foo: 0,
+      Bar: 0,
+      Bazz: 0
+    },
+    Larry: {
+      Foo: 0,
+      Bar: 0,
+      Bazz: 0
+    },
+    Curly: {
+      Foo: 0,
+      Bar: 0,
+      Bazz: 0
+    }
+  },
+  prizes: {
+    Foo: 1,
+    Bar: 3,
+    Bazz: 5
+  }
+};
+
+
+console.log('I have been loaded');
